03023a55fb6656446ba45bac5756b83c42741f44
IdlyBond <53187706+IdlyBond@users.noreply.github.com>
2022-08-03 18:02:40 +0100

    Implemented Add functionality

--- Assets/Scripts/Inventory/InventoryUtils.cs
+++ Assets/Scripts/Inventory/InventoryUtils.cs
@@ -1,7 +1,20 @@
-﻿namespace Inventory
+﻿using System.Collections.Generic;
+using System.Net.Http.Headers;
+using Inventory.Logic;
+
+namespace Inventory
 {
     public static class InventoryUtils
     {
-        public static readonly int NaNItemId = -1;
+        public static List<InventorySlot> CreateDefaultInventory(int size)
+        {
+            var slots = new List<InventorySlot>();
+            for (var i = 0; i < size; i++)
+            {
+                slots.Add(new InventorySlot(i));
+            }
+
+            return slots;
+        }
     }
 }
--- Assets/Scripts/Inventory/Logic/InventoryManager.cs
+++ Assets/Scripts/Inventory/Logic/InventoryManager.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
 using Core.ManagersSystem;
+using Inventory.Configs;
 using Utils.Debug;
 
 namespace Inventory.Logic
@@ -18,23 +21,84 @@ namespace Inventory.Logic
 
         public InventoryManager(int size)
         {
-            _slots = new List<InventorySlot>(size);
-            for (var i = 0; i < size; i++)
-            {
-                _slots.Add(new InventorySlot(i));
-            }
+            _slots = InventoryUtils.CreateDefaultInventory(size);
         }
 
         public void SetCurrentSlot(int slotId)
         {
-            if (slotId < 0 || slotId >= _slots.Count)
+            if (!CheckValidSlotId(slotId))
             {
-                Debug.LogWarning(this, $"Slot {slotId} in not existing. Aborting");
                 return;
             }
             
             _currentSlot = slotId;
             OnCurrentSlotChanged?.Invoke();
+        }
+
+        public void AddItem(ItemInfo item, int quantity)
+        {
+            if (FirstSlot(item) == default && !HasEmptySlot())
+            {
+                Debug.LogWarning(this, "No empty slots to add. Aborting");
+                return;
+            }
+            AddItem(item, FirstEmptySlot(), quantity);
+        }
+        
+        public void AddItemToSlot(ItemInfo item, int slotId, int quantity = 1)
+        {
+            if (!CheckValidSlotId(slotId))
+            {
+                return;
+            }
+
+            var slot = GetSlot(slotId);
+            if (slot.HasItem() && !slot.HasItem(item))
+            {
+                Debug.LogWarning(this, $"Trying to add wrong item type {item.Id}. Aborting");
+                return;
+            }
+            AddItem(item, slot, quantity);
+        }
+
+        private void AddItem(ItemInfo item, InventorySlot slot, int quantity)
+        {
+            slot.PutItems(item, quantity);
+        }
+
+        private bool HasEmptySlot()
+        {
+            return FirstEmptySlot() == default;
+        }
+
+        private InventorySlot FirstEmptySlot()
+        {
+            return _slots.FirstOrDefault(slot => !slot.HasItem());
+        }
+        
+        private InventorySlot FirstSlot(ItemInfo item)
+        {
+            return _slots.FirstOrDefault(slot => slot.HasItem() && item.Equals(slot.Item));
+        }
+
+        private InventorySlot GetSlot(int slotId)
+        {
+            return _slots.FirstOrDefault(slot => slot.Id == slotId);
+        }
+        
+        private bool CheckValidSlotId(int slotId)
+        {
+            if (IsValidSlotId(slotId))
+            {
+                return true;
+            }
+            Debug.LogWarning(this, $"Slot {slotId} in non existing. Aborting");
+            return false;
+        }
+
+        private bool IsValidSlotId(int slotId)
+        {
+            return slotId >= 0 && slotId < SlotsCount;
         }
         
     }
