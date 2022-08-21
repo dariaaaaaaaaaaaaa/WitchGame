6d1e2e177c549cb71f78fcdd4689e1a5b1463709
dariaaaaaaaaaaaaa <101401301+dariaaaaaaaaaaaaa@users.noreply.github.com>
2022-08-21 20:11:30 +0300

    hEALTH AND mANA dONE

--- Assets/Scripts/HealthAndMana/Health.cs
+++ Assets/Scripts/HealthAndMana/Health.cs
@@ -7,7 +7,8 @@ public class Health : MonoBehaviour
 {
     public int currentHealth;
     public int maxHealth;
-    public HealthBar healthBar; 
+    public HealthBar healthBar;
+    public int howManyHealht;
    
 
 
@@ -24,12 +25,23 @@ public class Health : MonoBehaviour
             TakeDamage(20);
         }
 
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            AddHealht();
+        }
+
     }
 
 
     private void TakeDamage(int damage)
     {
         currentHealth -= damage;
+        healthBar.SetHealth(currentHealth);
+    }
+
+    private void AddHealht()
+    {
+        currentHealth = currentHealth + howManyHealht;
         healthBar.SetHealth(currentHealth);
     }
 }
--- Assets/Scripts/HealthAndMana/Mana.cs
+++ Assets/Scripts/HealthAndMana/Mana.cs
@@ -1,18 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Mana : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public int currentMana;
+    public int maxMana;
+    public int howManyMana;
+    public ManaBar manaBar;
+
+
+
+
+    private void Start()
     {
-        
+        currentMana = maxMana;
+      
+        manaBar.SetMaxMana(maxMana);
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            AddMana();
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.Minus))
+        {
+            RemoveMana();
+
+        }
+
+    }
+    private void AddMana()
+    {
+       currentMana = currentMana + howManyMana;
+        manaBar.SetMana(currentMana);
+    }
+
+    private void RemoveMana()
+    {
+       currentMana = currentMana - howManyMana;
+      
+        manaBar.SetMana(currentMana);
     }
 }
